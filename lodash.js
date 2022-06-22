f61ad24df40b96997aef66f41d541ce8763fc725
Smartsam125 <samsonmujabi125@gmail.com>
2022-06-22 15:18:49 +0300

    Secon commit

--- lodash.js
+++ lodash.js
@@ -2,3 +2,15 @@ const _ =require('lodash')
 const items =[1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+const k = 6;
+const promise = new Promise((reolve,reject)=>{
+    const N =Math.floor(1 + Math.random()*6)
+    if (N===k) {
+        resolve("result")
+    }else{
+        reject('failed')
+    }
+})
+promise.catch((err)=>{
+    console.log(err);
+})
